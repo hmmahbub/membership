8d323e0909754d0187c1cfd23e0da42e9063885b
hmmahbub <maruf01744235920@gmail.com>
2023-09-21 15:19:04 +0600

    rmeove navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import './App.css'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 import LineChart from './components/lineChart/LineChart'
 import NavBar from './components/navBar/navBar'
-// import DaisiNav from './components/daisiNav/DaisiNav'
 
 function App() {
 
   return (
     <>
-      {/* <DaisiNav></DaisiNav> */}
       <NavBar></NavBar>
-      {/* <h1 className='font-bold text-4xl'>Vite + React</h1> */}
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
     </>
